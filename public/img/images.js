31bf5ca3d6475890886baa8d0a54d3c2d0c683b4
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-08-12 00:37:55 +0530

    version bug with node js fixed

---
+++ public/img/images.js
@@ -0,0 +1,17 @@
+import cover from './cover.jpg';
+import helmet from './helmet.jpg';
+import potion from './potion.jpg';
+import ring from './ring.jpg';
+import scroll from './scroll.jpg';
+import shield from './shield.jpg';
+import sword from './sword.jpg';
+
+export const images ={
+    cover,
+    helmet,
+    potion,
+    ring,
+    scroll,
+    shield,
+    sword
+};
--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import './App.css'
+import images from '../public/img/images.js'
 
 function App() {
   return (
