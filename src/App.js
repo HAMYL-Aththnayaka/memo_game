3e41a7a6bddf59bcdaf679e33202e44c43a669aa
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-08-12 01:42:54 +0530

    card grid layout created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'
-import images from '../public/img/images.js'
+import images from '../src/assets/img/images.js'
 
 function App() {
+  const [cards, setCard] = useState([]);
+  const [turns ,setTurns] =useState(0);
+
+
+  const suffeleCards = () => {
+
+    const suffledCards = [...images, ...images]
+      .sort(() => Math.random() - 0.5)
+      .map((image) => ({
+        image:image,
+        id: Math.random()
+      }));
+    setCard(suffledCards);
+    setTurns(0);
+  }
+console.log(cards,turns)
+
+
   return (
     <div className="App">
       <h1>memo __ magic</h1>
-      <button>New Game</button>
+      <button onClick={suffeleCards}>New Game</button>
+    
+    <div className="card-grid"></div>
+    {cards.map((card)=>(
+      <div className="card" key={card.id}>
+        <div >
+          <img className="front" src={card.src} alt="card front"/>
+        </div>
+      </div>
+    ))}
     </div>
   );
 }
---
+++ src/assets/img/images.js
@@ -0,0 +1,19 @@
+import cover from './cover.png';
+import helmet from './helmet-1.png';
+import potion from './potion-1.png';
+import ring from './ring-1.png';
+import scroll from './scroll-1.png';
+import shield from './shield-1.png';
+import sword from './sword-1.png';
+
+const images =[
+{   src:cover,
+    src:helmet,
+    src:potion,
+    src:ring,
+    src:scroll,
+    src:shield,
+    src:sword}
+];
+
+export default images;
